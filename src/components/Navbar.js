335466adbe4a545bf4174ea38c91e54834f560a4
Pernilla <coolanilla@hotmail.com>
2021-02-22 17:56:35 +0100

    Added content to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,13 @@
+import { NavLink } from 'react-router-dom';
+
 function Navbar() {
 
     <div className="navbar">
-        
+        <h1>Simple TODO</h1>
+        <div className="links">
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/new-todo">New TODO</NavLink>
+        </div>
     </div>
 
 }
